9ed89ae414c96daab25a77db3f11112b43c1919b
sonnysam <samuelagbenyo067@gmail.com>
2022-12-30 09:40:46 +0000

    pwd toggle

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import tw from "twrnc";
+import { Entypo } from "@expo/vector-icons";
 
 export default function App() {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.loginCont}>
+        <Text style={tw`text-xl font-bold text-center mb-2`}>
+          Password Visibility Toggle
+        </Text>
+        <View
+          style={[styles.input, tw`flex flex-row items-center justify-between`]}
+        >
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#000"
+            value={password}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={showPassword}
+            onChangeText={(text) => setPassword(text)}
+          />
+          <TouchableOpacity onPress={togglePassword}>
+            {showPassword ? (
+              <Entypo name="eye" size={24} color="black" />
+            ) : (
+              <Entypo name="eye-with-line" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +50,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#B5D5C5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginCont: {
+    width: "80%",
+    marginTop: 30,
+  },
+  input: {
+    width: "100%",
+    height: 55,
+    borderColor: "#fff",
+    borderWidth: 1,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderTopWidth: 1,
+    marginTop: 12,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    fontSize: 17,
+    color: "#000",
+    backgroundColor: "#fff",
   },
 });
